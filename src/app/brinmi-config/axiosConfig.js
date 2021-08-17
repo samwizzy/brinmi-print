ac293448de9e83d96c32c83455bf3136bab15f06
Samuel Okeke <samwizzy11@gmail.com>
2021-08-17 14:22:26 +0100

    updated axios & _redirects

--- src/app/brinmi-config/axiosConfig.js
+++ src/app/brinmi-config/axiosConfig.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = "http://134.209.64.28:8082/";
+const baseURL = 'http://134.209.64.28:8082/';
+
+const token = localStorage.getItem('token');
 
 axios.defaults.baseURL = baseURL;
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
-axios.defaults.headers.common["Authorization"] = localStorage.getItem("token")
-  ? "Bearer " + localStorage.getItem("token") : "";
-axios.defaults.headers.common["X-Frame-Options"] = "sameorigin";
 
-console.log(axios.defaults.headers.common, "axios.defaults.common");
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+axios.defaults.headers.common['X-Frame-Options'] = 'sameorigin';
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
